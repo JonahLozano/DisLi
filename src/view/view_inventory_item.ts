6404cbaca42db4ba13ef17561d6ec1502b62854b
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-26 16:30:21 -0700

    view_inventory_item.ts complete

--- src/view/view_inventory_item.ts
+++ src/view/view_inventory_item.ts
@@ -21,5 +21,41 @@ export = class create_inventory_item_view {
         elementType: "divider",
       });
     }
+
+    addItem(
+      id: string,
+      serial_number: string,
+      status: string,
+      brand: string,
+      model: string,
+      code_name: string
+    ) {
+      this.data.content[1].content.push({
+          "elementType": "blockHeading",
+          "heading": `${brand} ${model}`,
+          "headingTextAlignment": "center",
+          "headingLevel": 1
+        },
+        {
+          "elementType": "nameTag",
+          "id": `${id}`,
+          "nameTagStyle": "vertical",
+          "name": `${code_name}`,
+          "description": `${serial_number}`,
+          "imageHeight": "xlarge",
+          "imageWidth": "xlarge",
+          "imageBorderWidth": "2px",
+          "imageBorderColor": "black",
+          "image": {
+            "url": "https://i.ibb.co/G9LC6hK/devices.png",
+            "alt": "Device Image",
+            "cropStyle": "fill"
+          }
+        },
+        {
+          "elementType": "html",
+          "html": `<p style="text-align: center;">Device Status: ${status}</p>`
+      });
+    }
 };
   
