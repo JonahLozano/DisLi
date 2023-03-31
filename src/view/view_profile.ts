9a334cccdf48cd1ade34fa3fbdcd61c6f95ea8b3
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-30 18:33:11 -0700

    Redesigned view_profile.ts

--- src/view/view_profile.ts
+++ src/view/view_profile.ts
@@ -7,186 +7,150 @@ export = class create_profile_view {
         },
         "contentContainerWidth": "narrow",
         "content": [
-            {
-                "elementType": "blockHeading",
-                "heading": "My Profile",
-                "headingTextAlignment": "center",
-                "headingLevel": 1
-            },
-            {
-                "elementType": "divider",
-                "borderStyle": "solid",
-                "borderWidth": "3px",
-                "borderColor": "black"
-            },
-            {
-                "elementType": "divider",
-                "borderColor": "transparent"
-            },
-            {
-                "elementType": "list",
-                "id": "profileList",
-                "itemSize": "medium",
-                "listStyle": "separated",
-                "titleFontSize": "xlarge",
-                "titleTextColor": "white",
-                "accessoryColor": "white",
-                "backgroundColor": "navy",
-                "borderRadius": "none",
-                "items": [
-                    {
-                        "title": "Check Out Device",
-                        "link": {
-                            "relativePath": ""
-                        },
-                        "image": {
-                            "url": "https://i.ibb.co/TMpxVGP/shopping.png",
-                            "alt": "Shopping Cart Image"
-                        },
-                        "imageWidth": "5rem",
-                        "imageHeight": "5rem"
-                    },
-                    {
-                        "title": "Lost / Broken Device Form",
-                        "link": {
-                            "external": "https://docs.google.com/forms/d/e/1FAIpQLSdFzMoX9i8MR0JEOgp5eddefcTxZQgZZC3ud1T25LkNeE1c3A/viewform?usp=sf_link"
-                        },
-                        "image": {
-                            "url": "https://i.ibb.co/NrHQq0s/brokendevice.png",
-                            "alt": "Broken Device Image"
-                        },
-                        "imageWidth": "5rem",
-                        "imageHeight": "5rem"
-                    }
-                ]
-            },
-            {
-                "elementType": "divider",
-                "borderColor": "transparent"
-            },
-            {
-                "elementType": "collapsible",
-                "disclosureIcon": "chevron",
-                "disclosureIconColor": "white",
-                "title": "My Appointments",
-                "titleTextColor": "white",
-                "headingBackgroundColor": "navy",
-                "borderTopStyle": "solid",
-                "borderTopColor": "navy",
-                "borderTopWidth": "1px",
-                "borderBottomStyle": "solid",
-                "borderBottomColor": "navy",
-                "borderBottomWidth": "2px",
-                "titleLineHeight": "55px",
+          {
+            "elementType": "blockHeading",
+            "heading": "My Profile",
+            "headingTextAlignment": "center",
+            "headingLevel": 1
+          },
+          {
+            "elementType": "list",
+            "id": "profileList",
+            "itemSize": "medium",
+            "listStyle": "separated",
+            "titleFontSize": "xlarge",
+            "titleTextColor": "white",
+            "accessoryColor": "white",
+            "backgroundColor": "navy",
+            "borderColor": "transparent",
+            "items": [
+              {
+                "title": "Check Out Device",
+                "link": {
+                  "relativePath": ""
+                },
                 "image": {
-                    "url": "https://i.ibb.co/J3Bq14d/appoint.png",
-                    "alt": "Appointments Image"
+                  "url": "https://i.ibb.co/TMpxVGP/shopping.png",
+                  "alt": "Shopping Cart Image"
                 },
                 "imageWidth": "5rem",
-                "imageHeight": "4rem",
-                "content": [
-                    {
-                        "elementType": "table",
-                        "id": "appointmentsTable",
-                        "columnHeaders": true,
-                        "outerBorderWidth": "2px",
-                        "rowBorderWidth": "2px",
-                        "columnBorderWidth": "2px",
-                        "outerBorderColor": "navy",
-                        "rowBorderColor": "navy",
-                        "columnBorderColor": "navy",
-                        "paddingTop": "none",
-                        "paddingBottom": "xtight",
-                        "columnOptions": [
-                            {
-                                "horizontalAlign": "center",
-                                "verticalAlign": "middle"
-                            },
-                            {
-                                "horizontalAlign": "center",
-                                "verticalAlign": "middle"
-                            },
-                            {
-                                "horizontalAlign": "center",
-                                "verticalAlign": "middle"
-                            }
-                        ],
-                        "rows": [
-                          {
-                            "cells": [
-                              {
-                                "title": "#"
-                              },
-                              {
-                                "title": "Name"
-                              },
-                              {
-                                "title": "Date Time"
-                              }
-                            ]
-                          },
-                        ]
-                    }
-                ]
-            },
-            {
-                "elementType": "divider",
-                "borderColor": "transparent"
-            },
-            {
-                "elementType": "collapsible",
-                "disclosureIcon": "chevron",
-                "disclosureIconColor": "white",
-                "title": "My Devices",
-                "titleTextColor": "white",
-                "headingBackgroundColor": "navy",
-                "borderTopStyle": "solid",
-                "borderTopColor": "navy",
-                "borderTopWidth": "1px",
-                "borderBottomStyle": "solid",
-                "borderBottomColor": "navy",
-                "borderBottomWidth": "2px",
-                "titleLineHeight": "55px",
-                "image": {
-                    "url": "https://i.ibb.co/tXbJwXL/devices.png",
-                    "alt": "Devices Image"
+                "imageHeight": "5rem"
+              },
+              {
+                "title": "Lost / Broken Device Form",
+                "link": {
+                    "external": "https://docs.google.com/forms/d/e/1FAIpQLSdFzMoX9i8MR0JEOgp5eddefcTxZQgZZC3ud1T25LkNeE1c3A/viewform?usp=sf_link"
                 },
-                "imageWidth": "4rem",
-                "imageHeight": "4rem",
-                "content": [
-                    {
-                        "elementType": "cardSet",
-                        "id": "imageStyle_thumbnailSmall",
-                        "marginTop": "medium",
-                        "borderColor": "navy",
-                        "borderWidth": "2px",
-                        "marginBottom": "medium",
-                        "items": []
-                    }
+                "image": {
+                  "url": "https://i.ibb.co/NrHQq0s/brokendevice.png",
+                  "alt": "Broken Device Image"
+                },
+                "imageWidth": "5rem",
+                "imageHeight": "5rem"
+              }
+            ]
+          },
+          {
+            "elementType": "container",
+            "wrapperStyle": "subfocal",
+            "margin": "none",
+            "padding": "medium",
+            "content": [
+              {
+                "elementType": "blockHeading",
+                "heading": "My Appointments",
+                "headingTextAlignment": "center",
+                "headingLevel": 3
+              },
+              {
+                "elementType": "table",
+                "id": "appointmentsTable",
+                "columnHeaders": true,
+                "outerBorderWidth": "2px",
+                "rowBorderWidth": "2px",
+                "columnBorderWidth": "2px",
+                "outerBorderColor": "navy",
+                "rowBorderColor": "navy",
+                "columnBorderColor": "navy",
+                "paddingTop": "none",
+                "paddingBottom": "xtight",
+                "columnOptions": [
+                  {
+                    "horizontalAlign": "center",
+                    "verticalAlign": "middle"
+                  },
+                  {
+                    "horizontalAlign": "center",
+                    "verticalAlign": "middle"
+                  },
+                  {
+                    "horizontalAlign": "center",
+                    "verticalAlign": "middle"
+                  }
+                ],
+                "rows": [
+                  {
+                    "cells": [
+                      {
+                        "title": "#"
+                      },
+                      {
+                        "title": "Name"
+                      },
+                      {
+                        "title": "Date / Time"
+                      }
+                    ]
+                  },
                 ]
-            }
+              }
+            ]
+          },
+          {
+            "elementType": "divider",
+            "borderColor": "transparent"
+          },
+          {
+            "elementType": "container",
+            "wrapperStyle": "subfocal",
+            "margin": "none",
+            "padding": "medium",
+            "content": [
+              {
+                "elementType": "blockHeading",
+                "heading": "My Devices",
+                "headingTextAlignment": "center",
+                "headingLevel": 3
+              },
+              {
+                "elementType": "cardSet",
+                "id": "imageStyle_thumbnailSmall",
+                "marginTop": "medium",
+                "borderColor": "navy",
+                "borderWidth": "2px",
+                "marginBottom": "medium",
+                "items": [
+
+                ]
+              }
+            ]
+          }
         ]
-    }
+      }
     }
   
     getData() {
       return this.data;
-    }
-  
-    addDivider() {
-      this.data.content[1].content.push({
-        elementType: "divider",
-      });
     }
 
     addTableRow(
       university_id: string,
       checkout_date: Date
     ) {
-      this.data.content[1].content.push({
+      this.data.content[2].content[1].rows.push({
         cells: [
           {
-            title: "Number goes here"
+            title: "Number Index"
           },
           {
             title: `${university_id}`
@@ -204,7 +168,7 @@ export = class create_profile_view {
       code_name: string,
       return_date: string
     ) {
-      this.data.content[1].content.push({
+      this.data.content[4].content[1].items.push({
         elementType: "contentCard",
         size: "small",
         imageStyle: "thumbnailSmall",
