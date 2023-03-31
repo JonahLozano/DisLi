e8b1a003df1bf656da28c4cdd92ef53eee3366fe
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-30 17:34:33 -0700

    created view profile checkout file

---
+++ src/view/view_profile_checkout.ts
@@ -0,0 +1,21 @@
+export = class create_profile_checkout_view {
+    data: any;
+    constructor() {
+        this.data = {
+            "metadata": {
+                "version": "2.0"
+            },
+            "contentContainerWidth": "narrow",
+            "content": [
+                
+            ]
+        }
+    }
+  
+    getData() {
+      return this.data;
+    }
+
+    
+};
+  
