c6b53e7fdf20b0801add605f4075f8d8c36c5a4b
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2023-03-31 15:32:54 -0700

    added new routes for testing

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import Application from "./routes/application";
 import Help from "./routes/help";
 import Inventory from "./routes/inventory";
 import Program from "./routes/program";
+import Dev from "./utils/dev";
+import Home from "./utils/home";
 
-const routes =  (app: Express) => {
+const routes = (app: Express) => {
   app.use("/application", Application);
   app.use("/help", Help);
   app.use("/inventory", Inventory);
   app.use("/program", Program);
-}
+  app.use("/dev", Dev);
+  app.use("/", Home);
+};
 
 export default routes;
