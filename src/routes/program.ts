d1fbacee2d8dac98e2e4404222367990abfff3e4
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-04-18 22:21:01 -0700

    program route changed temporarily

--- src/routes/program.ts
+++ src/routes/program.ts
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
-import program_logic from "../logic/program_logic";
+import program_logic from "../logic/program_logic_2";
 
 router.get("/", program_logic.view_programs);
 
-router.post("/", program_logic.add_program);
+//router.post("/", program_logic.add_program);
 
-router.delete("/", program_logic.delete_program);
+//router.delete("/", program_logic.delete_program);
 
-router.put("/", program_logic.end_program);
+//router.put("/", program_logic.end_program);
 
 export = router;
