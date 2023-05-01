81c38fb144d021fe2e09bb07c2ddf20785550cc2
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-04-30 21:21:56 -0700

    changed to inventory logic

--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -1,9 +1,29 @@
 import express from "express";
 const router = express.Router();
+import inventory_logic from "../logic/inventory_logic";
 // import authorize_page from "../middleware/authorize_page";
 // import { UserRole } from "../utils/UserRoles";
-import inventory_logic from "../logic/inventory_logic";
-import person_logic = require("../logic/person_logic");
+//import person_logic = require("../logic/person_logic");
+
+router.get("/", inventory_logic.view_inventory);
+
+router.get("/:id", inventory_logic.view_item);
+
+router.get("/add", inventory_logic.view_add_item);
+
+router.post("/add", inventory_logic.add_item);
+
+router.put("/", inventory_logic.modify_item);
+
+/*
+router.get("/report", inventory_logic.report_item);
+
+router.get("/search", inventory_logic.search_item);
+
+router.get("/checkouts", person_logic.view_all_checkout);
+
+router.get("/approve", person_logic.approve_checkout);
+*/
 
 // router.get(
 //   "/",
@@ -16,23 +36,5 @@ import person_logic = require("../logic/person_logic");
 // router.post("/", authorize_page([UserRole.ADMIN]), inventory_logic.add_item);
 
 // router.put("/", authorize_page([UserRole.ADMIN]), inventory_logic.modify_item);
-
-router.get("/", inventory_logic.view_inventory);
-
-router.get("/:id", inventory_logic.view_item);
-
-router.get("/add", inventory_logic.view_add_item);
-
-router.post("/add", inventory_logic.add_item);
-
-router.put("/", inventory_logic.modify_item);
-
-router.get("/report", inventory_logic.report_item);
-
-router.get("/search", inventory_logic.search_item);
-
-router.get("/checkouts", person_logic.view_all_checkout);
-
-router.get("/approve", person_logic.approve_checkout);
 
 export = router;
